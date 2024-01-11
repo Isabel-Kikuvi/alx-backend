7cdbe55cdc72cb9a36a874b66f7968089dfcf10f
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-01-11 01:34:53 -0800

    minor edits

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,34 +1,33 @@
-#!/usr/bin/env python3
-"""Create a class FIFOCache that inherits from
-BaseCaching and is a caching system:
+#!/usr/bin/python3
+""" FIFOCache module
 """
 
-
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """BasicCache class that inherits from BaseCaching"""
+    """ FIFOCache class that inherits from BaseCAching"""
 
     def __init__(self):
-        """summary"""
+        """ Initialize FIFOCache"""
         super().__init__()
 
     def put(self, key, item):
-        """summary"""
+        """ Add an item in the cache using FIFO algorithm"""
         if key is None or item is None:
-            pass
-        else:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
-                    and key not in self.cache_data.keys():
-                first_key = next(iter(self.cache_data.keys()))
-                del self.cache_data[first_key]
-                print("DISCARD: {}". format(first_key))
+            return
 
-            self.cache_data[key] = item
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            # Get the first item inserted (oldest)
+            first_key = next(iter(self.cache_data))
+            del self.cache_data[first_key]
+            print("DISCARD: {}".format(first_key))
+
+        self.cache_data[key] = item
 
     def get(self, key):
-        """return the value in self.cache_data linked to key"""
-        if key is None or key not in self.cache_data.keys():
+        """ Get an item by key from the cache"""
+        if key is None or key not in self.cache_data:
             return None
-        return self.cache_data.get(key)
+
+        return self.cache_data[key]
