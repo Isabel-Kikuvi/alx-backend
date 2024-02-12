a208091c638736ca046d79ecc4c98b0251a2f76d
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-02-12 06:12:07 -0800

    task 5

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,24 @@
+import { createClient } from 'redis';
+
+
+const client = createClient();
+
+
+client.on('error', err => console.log(`Redis client not connected to the server: ${err.message}`));
+client.on('connect', () => {
+  console.log('Redis client connected to the server')
+  client.subscribe('holberton school channel');
+});
+         
+client.connect;
+
+const listener = (channel, message) => {
+  console.log(message)
+  if (message == 'KILL_SERVER') {
+    client.unsubscribe(channel);
+    client.disconnect;
+    process.exit();
+  }
+}
+
+client.on('message', listener)
