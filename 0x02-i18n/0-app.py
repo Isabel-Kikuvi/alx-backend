0be5aadd1ce221f79b3ed8f2e6f719c939956741
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-03-12 13:18:48 +0300

    Pep8

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """ Basic Flask App"""
+
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    """Method that renders the index template"""
+    """Method renders the index template"""
     return render_template('0-index.html')
 
 
