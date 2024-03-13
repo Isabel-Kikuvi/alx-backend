3ad924566a1ac105ee7782cfe7b956f6121c4bc5
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-03-13 07:04:04 +0300

    Instantiated Babel

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,9 +2,13 @@
 """ Basic Flask App"""
 
 from flask import Flask, render_template
+from flask_babel import Babel
+from config import Config
 
 
 app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
 
 
 @app.route('/')
