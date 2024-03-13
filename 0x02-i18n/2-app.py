cd68f8382a829c2832b46f2b417046750fd7385f
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-03-13 08:08:08 +0300

    Locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -14,15 +14,21 @@ class Config(object):
 
 app = Flask(__name__)
 babel = Babel(app)
+app.url_map.strict_slashes = False
 app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Determines the best locale based on user pref and supported lang."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
     """Method renders the index template"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
     app.run()
- 
