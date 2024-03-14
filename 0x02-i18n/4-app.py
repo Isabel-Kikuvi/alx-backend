056c63486c421f25071a62f1136c9e28cc51d4be
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-03-14 13:15:04 +0300

    Task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-""" Simple flask application """
+"""Simple flask application
+"""
+
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config(object):
-    """ Configuring language and timezones
+    """Configuring language and timezones
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
@@ -21,17 +24,22 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """ Determines the best locale based on user pref and supported lang.
+    """Determines the best locale based on user pref and supported lang. 
     """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        print(locale)
+        return locale
+
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def hello():
+def index():
     """Method renders the index template
     """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
-    app.run(port="5000", host='0.0.0.0', debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
